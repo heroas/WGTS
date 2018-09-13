bc9a86ed5da210718645fff5245103460ad539d7
heroas <heroas@protagonist.xyz>
2018-09-13 10:37:42 -0400

    added build files for pyinstallerhook and asyncgui tools

---
+++ build_files/hook-kivymd.py
@@ -0,0 +1,13 @@
+# file goes to pyinstaller/hooks
+# will be recognized by pyinstaller, no need for hidden modules
+
+from PyInstaller.utils.hooks import (
+    collect_data_files, 
+    copy_metadata,
+    collect_submodules
+)
+
+datas = copy_metadata('kivymd')
+hiddenimports = collect_submodules('kivymd')
+
+datas = collect_data_files('kivymd')
