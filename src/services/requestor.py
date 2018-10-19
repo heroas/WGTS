f2fff542df1570c307c6809aeb0c1f6c9d7b5f80
Hero <heroas@protagonist.xyz>
2018-10-18 20:29:47 -0400

    dont need requests.futures. trying to package app

--- src/services/requestor.py
+++ src/services/requestor.py
@@ -1,26 +1,12 @@
 import requests
-from requests_futures.sessions import FuturesSession
-
-session = FuturesSession()
 
 def get_json_from_get(url):
     response = requests.get(url)
     return response.json()
 
-def bg_cb(sess, resp):
-    # parse the json storing the result on the response object
-    resp.data = resp.json()
-
 def get_json_from_post(url, data):
-    #future = session.post(url, json = data)
-    #response = requests.post(url, json = data)
-
-    future = session.post(url,json=data, background_callback=bg_cb)
-    # do some other stuff, send some more requests while this one works
-    response = future.result()
-    print(response.data)
-    # data will have been attached to the response object in the background
-    return response.data
+    result = requests.post(url,json=data);
+    return result.json()
 
 def get_json_for_graphql(query, variables):
     return {'query': query, 'variables': variables}
