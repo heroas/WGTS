2c47100e837a75c9291cb757d9f918d3aa34db1e
heroas <hero@protagonist.xyz>
2018-11-18 11:11:54 -0500

    setup for development script

---
+++ build_files/setup_dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#clone repos needed
+git clone https://gitlab.com/kivymd/KivyMD
+
+virtualenv -p python3 wgts
+#source wgts/bin/activate
+wgts/bin/pip install -r WGTS/build_files/requirements.txt
+wgts/bin/pip install kivy
+
+cd KivyMD
+../wgts/bin/python ./setup.py install
+
+cd ..
+
+cp WGTS/build_files/kivy.py wgts/lib/python3.6/site-packages/async_gui/toolkits/
